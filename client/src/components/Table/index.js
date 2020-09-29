31e683329b195f38adcd82d86d3513866b38f42d
Bryce Speirs <baspeirs@gmail.com>
2020-09-29 15:32:18 -0400

    missed file

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -12,14 +12,17 @@ export default function Table(props) {
     })
 
     useEffect(() => {
+        // arrays to store employees by type
         let managerArr = [];
         let driverArr = [];
         let inshopArr = [];
+        // loop to push employees to proper array
         props.employees.forEach(employee => {
             if (employee.type === "Manager") { managerArr.push(employee) }
             else if (employee.type === "Driver") driverArr.push(employee)
             else if (employee.type === "Inshop") inshopArr.push(employee)
             else console.log("employee issue, fix things");
+            // set state with the employee arrays
             setEmployeeState({
                 managers: managerArr,
                 drivers: driverArr,
@@ -27,23 +30,24 @@ export default function Table(props) {
             });
         });
     }, [])
-    const test = () => {
-        console.log(props)
-        console.log(employeeState)
-    }
     return (
         <div className="col-lg-3">
-            <button onClick={test}>click me</button>
             <table className="table table-striped table-dark">
                 <TableHead day={props.day} />
+
+                {/* creates a table row for the manager section */}
                 <h4>Manager</h4>
                 {employeeState.managers.map(manager => (
                     <ManagerTableRow manager={manager}/>
                 ))}
+
+                {/* creates a table row for the in shop section */}
                 <h4>In-Shop</h4>
                 {employeeState.inshops.map(inshop => (
                     <InShopTableRow inshop={inshop}/>
                 ))}
+
+                {/* creates a table row for the driver section */}
                 <h4>Driver</h4>
                 {employeeState.drivers.map(driver => (
                 <DriverTableRow driver={driver}/>
