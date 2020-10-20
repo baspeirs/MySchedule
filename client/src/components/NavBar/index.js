5cb72f54adb0bcb8f6cb91c91bd85f9d8f635f53
Bryce Speirs <baspeirs@gmail.com>
2020-10-20 13:05:25 -0400

    navbar styling finished

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Logo from "../../images/Logo.png";
 import { Link } from 'react-router-dom';
 
 export default function NavBar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">Welcome {props.authState.user.name}</a>
+            <a className="navbar-brand" href="#"><img alt="logo" src={Logo} style={{width: "50px"}}/> MySchedule</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item"><Link to="/home">Home</Link></li>
-                    <li className="nav-item"><Link to ="timeoff">Time Off</Link></li>
-                    <li className="nav-item"><Link to="manageroperations">Manager Ops</Link></li>
+                    <Link to="/home"><li className="nav-item"><a className="nav-link" href="#">Home</a></li></Link>
+                    <Link to ="timeoff"><li className="nav-item"><a className="nav-link" href="#">Time Off</a></li></Link>
+                    <Link to="manageroperations"><li className="nav-item"><a className="nav-link" href="#">Manager Ops</a></li></Link>
                     <li className="nav-item"><a className="nav-link" href="#" onClick={props.logout}>Log Out</a></li>
                 </ul>
                 <span className="navbar-text">
